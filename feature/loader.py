14ce68e85535d187fbbef98e8e981ea10302511b
Galen Han <Mastergalen@users.noreply.github.com>
2018-04-01 23:27:25 +0200

    SVM :100: :open_mouth:

---
+++ feature/loader.py
@@ -0,0 +1,52 @@
+"""
+Load featured-based data
+"""
+import csv
+import numpy as np
+
+DATA_PATH = 'data/labelled_au.csv'
+FRAMES_PER_CLIP = 150
+
+
+def load(shuffle=True):
+    nb_lines = len(open(DATA_PATH).readlines()) - 1  # Subtract 1 for header
+    nb_samples = nb_lines // FRAMES_PER_CLIP
+    x = np.zeros((nb_samples, 150, 36))
+    y = np.zeros(nb_samples)
+    with open(DATA_PATH) as f:
+        reader = csv.DictReader(f)
+
+        for i, row in enumerate(reader):
+            sample_idx = i // FRAMES_PER_CLIP
+            frame_idx = i % FRAMES_PER_CLIP
+
+            if frame_idx == 0:
+                y[sample_idx] = row['isBluffing']
+
+            x[sample_idx, frame_idx] = list(row.values())[1:37]
+
+    # Unison shuffle
+    if shuffle:
+        print("Shuffling data")
+        idx = np.random.permutation(nb_samples)
+        return x[idx], y[idx]
+
+    return x, y
+
+
+def split_data(x, y):
+    """
+    Split data into training and validation set
+    :return:
+    """
+    training_percent = 0.7
+
+    split_idx = round(training_percent * len(y))
+
+    x_train = x[:split_idx]
+    x_val = x[split_idx:]
+
+    y_train = y[:split_idx]
+    y_val = y[split_idx:]
+
+    return x_train, y_train, x_val, y_val
